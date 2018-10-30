77cace43a323ee99071fa78b9f1b34e81494112a
Mike Rogers <me@mikerogers.io>
2018-10-30 22:02:13 +0100

    Making homepage work again - error still there

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,12 @@ end
 
 # TODO: Deleting these engines stops the error appearing
 MissionControl::Engine.routes.draw do
-  resources :projects
 end
 
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
+  resources :projects
   root to: 'projects#index'
 end
